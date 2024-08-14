28b6f821c97ba7fe08645d248817183200cb3ef5
tsuyuki <tsuyuki@example.com>
2024-08-14 05:55:36 +0000

    非ログインTopPageとログイン後TopPageを作成

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+CTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" contents="width=divice=width, initial-scale=1">
+        <title>ログイン機能付きタスク管理サービス @yield('title')</title>
+    </head>
+    <body>
+        @yield('contents')
+    </body>
+</html>
