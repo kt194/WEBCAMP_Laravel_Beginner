58614a4535e87f2df5562d1012c94d62f4fa8341
tsuyuki <tsuyuki@example.com>
2024-08-14 05:33:39 +0000

    非ログイントップページ作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" contents="width=divice=width, initial-scale=1">
+        <title>ログイン機能付きタスク管理サービス</title>
+    </head>
+    <body>
+        <!-- コンテンツ -->
+        <h1>ログイン</h1>
+        <form action="/login" method="post">
+            email：<input name="email"><br>
+            パスワード：<input type="password" name="password"><br>
+            <button>ログインする</button>
+        </form>
+    </body>
+</html>
