64b78adc349ba8fb77d9a6a42b47d9624a172d19
tsuyuki <tsuyuki@example.com>
2024-09-18 03:01:38 +0000

    タスクの削除と完了を書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,20 +7,22 @@ use Illuminate\Http\Request;
 use App\Http\Requests\TaskRegisterPostRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Task as TaskModel;
+use Illuminate\Support\Facades\DB;
+use App\Models\CompletedTask as CompletedTaskModel;
 
 class TaskController extends Controller
 {
     /**
      * タスク一覧ページ を表示する
-     * 
+     *
      * @return \Illuminate\View\View
      */
     public function list()
     {
-        
+
         // 1Page辺りの表示アイテム数を設定
-        $per_page = 2;
-        
+        $per_page = 5;
+
         // 一覧の取得
         $list = TaskModel::where('user_id', Auth::id())
                          ->orderBy('priority', 'DESC')
@@ -37,10 +39,10 @@ class TaskController extends Controller
         //echo "<pre>\n"; var_dump($sql, $list); exit;
         var_dump($sql);
         */
-        
+
         return view('task.list', ['list' => $list]);
     }
-    
+
     /**
      * タスクの新規登録
      */
@@ -71,7 +73,7 @@ class TaskController extends Controller
         // リダイレクト
         return redirect('/task/list');
     }
-    
+
     /**
      * タスクの詳細閲覧
      */
@@ -79,7 +81,7 @@ class TaskController extends Controller
     {
         return $this->singleTaskRender($task_id, 'task.detail');
     }
-    
+
     /**
      * タスクの編集画面表示
      */
@@ -87,7 +89,7 @@ class TaskController extends Controller
     {
         return $this->singleTaskRender($task_id, 'task.edit');
     }
-    
+
     /**
      * タスクの編集処理
      */
@@ -120,7 +122,73 @@ class TaskController extends Controller
         $request->session()->flash('front.task_edit_success', true);
         // 詳細閲覧画面にリダイレクトする
         return redirect(route('detail', ['task_id' => $task->id]));
-    } 
+    }
+
+    /**
+     * 削除処理
+     */
+    public function delete(Request $request, $task_id)
+    {
+        // task_idのレコードを取得する
+        $task = $this->getTaskModel($task_id);
+
+        // タスクを削除する
+        if ($task !== null) {
+            $task->delete();
+            $request->session()->flash('front.task_delete_success', true);
+        }
+
+        // 一覧に遷移する
+        return redirect('/task/list');
+    }
+
+     /**
+     * タスクの完了
+     */
+    public function complete(Request $request, $task_id)
+    {
+        /* タスクを完了テーブルに移動させる */
+        try {
+            // トランザクション開始
+            DB::beginTransaction();
+
+            // task_idのレコードを取得する
+            $task = $this->getTaskModel($task_id);
+            if ($task === null) {
+                // task_idが不正なのでトランザクション終了
+                throw new \Exception('');
+            }
+
+            // tasks側を削除する
+            $task->delete();
+            //var_dump($task->toArray()); exit;
+
+            // completed_tasks側にinsertする
+            $dask_datum = $task->toArray();
+            unset($dask_datum['created_at']);
+            unset($dask_datum['updated_at']);
+            $r = CompletedTaskModel::create($dask_datum);
+            if ($r === null) {
+                // insertで失敗したのでトランザクション終了
+                throw new \Exception('');
+            }
+            // echo '処理成功'; exit;
+
+            // トランザクション終了
+            DB::commit();
+            // 完了メッセージ出力
+            $request->session()->flash('front.task_completed_success', true);
+        } catch(\Throwable $e) {
+            // var_dump($e->getMessage()); exit;
+            // トランザクション異常終了
+            DB::rollBack();
+            // 完了失敗メッセージ出力
+            $request->session()->flash('front.task_completed_failure', true);
+        }
+
+        // 一覧に遷移する
+        return redirect('/task/list');
+    }
     
     /**
      * 「単一のタスク」Modelの取得
@@ -136,7 +204,7 @@ class TaskController extends Controller
         if ($task->user_id !== Auth::id()) {
             return null;
         }
-        
+
         return $task;
     }
 
